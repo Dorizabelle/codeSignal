e5add6abb5ef7eb6971eb942a8dd391d72533c7e
Dorizabelle Northecide <Dorizabellen@gmail.com>
2021-08-07 15:37:13 -0400

    CodeSignal: The Journey Begins: Add

---
+++ add.js
@@ -0,0 +1,38 @@
+// Dorizabelle    
+// https://www.linkedin.com/in/dorizabelle/
+// https://github.com/Dorizabelle
+// https://www.instagram.com/dorizabelle/
+
+//CodeSignal: The Journey Begins
+//Question 
+    // Write a function that returns the sum of two numbers.
+
+    // Example
+
+    // For param1 = 1 and param2 = 2, the output should be
+    // add(param1, param2) = 3.
+
+    // Input/Output
+
+    // [execution time limit] 4 seconds (js)
+
+    // [input] integer param1
+
+    // Guaranteed constraints:
+    // -1000 ≤ param1 ≤ 1000.
+
+    // [input] integer param2
+
+    // Guaranteed constraints:
+    // -1000 ≤ param2 ≤ 1000.
+
+    // [output] integer
+
+    // The sum of the two inputs.
+
+//My Code
+function add(param1, param2) {
+    var answer = param1+param2;
+    
+    return answer;
+}
